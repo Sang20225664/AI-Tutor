c4dd73d4e94aaa32a034455bc4ee1bd5692fd28f
TanSang <sang.ndt225664@sis.hust.edu.vn>
2025-05-20 23:49:11 +0700

    Thêm file .gitignore mới

--- ai_tutor_backend/src/models/lesson.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const lessonSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("lesson", lessonSchema);
--- ai_tutor_backend/src/routes/api.js
+++
@@ -1,8 +0,0 @@
-// routes/api.js
-const express = require('express');
-const router = express.Router();
-const geminiController = require('../controllers/geminiController');
-
-router.post('/chat', geminiController.chatWithGemini);
-
-module.exports = router;
